376acae8cfd6d074462b9dfa3c44cd0c2484602c
sonnemaf <fons.sonnemans@reflectionit.nl>
2017-01-12 15:20:35 +0100

    Add project files.

---
+++ App47/Behaviors/ScrollSelectedItemIntoViewBehavior.cs
@@ -0,0 +1,50 @@
+﻿using App47.Helpers;
+using Microsoft.Xaml.Interactivity;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App47.Behaviors {
+
+    public class ScrollSelectedItemIntoViewBehavior : Behavior<ListViewBase> {
+
+        protected override void OnAttached() {
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+            base.OnAttached();
+        }
+        protected override void OnDetaching() {
+            AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+            base.OnDetaching();
+        }
+
+        private async void AssociatedObject_SelectionChanged(object sender, 
+                                                        SelectionChangedEventArgs e) {
+            var item = e.AddedItems.FirstOrDefault();
+            if (item != null) {
+                if (IsScrollAnimated) {
+                    await this.AssociatedObject.ScrollToItemAsync(item);
+                } else {
+                    await this.AssociatedObject.ScrollIntoViewAsync(item);
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Get or Sets the IsScrollAnimated dependency property.  
+        /// </summary> 
+        public bool IsScrollAnimated {
+            get { return (bool)GetValue(IsScrollAnimatedProperty); }
+            set { SetValue(IsScrollAnimatedProperty, value); }
+        }
+
+        /// <summary> 
+        /// Identifies the IsScrollAnimated dependency property. 
+        /// This enables animation, styling, binding, etc...
+        /// </summary> 
+        public static readonly DependencyProperty IsScrollAnimatedProperty =
+            DependencyProperty.Register(nameof(IsScrollAnimated),
+                                typeof(bool),
+                                typeof(ScrollSelectedItemIntoViewBehavior),
+                                new PropertyMetadata(true));
+    }
+}
---
+++ App47/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App47 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+
+        public MainPage() {
+            this.InitializeComponent();
+        }
+
+        private void ButtonFirst_Click(object sender, RoutedEventArgs e) {
+            listViewProducts.SelectedIndex = 0;
+        }
+
+        private void ButtonMiddle_Click(object sender, RoutedEventArgs e) {
+            listViewProducts.SelectedIndex = (listViewProducts.Items.Count - 1) / 2;
+        }
+
+        private void ButtonLast_Click(object sender, RoutedEventArgs e) {
+            listViewProducts.SelectedIndex = listViewProducts.Items.Count - 1;
+
+        }
+    }
+}
